a47acec9ebacbfbbd25f8bfdad965897bc84fe5f
Lola Veries <102049157+lolaiuvt@users.noreply.github.com>
2022-03-23 11:58:47 -0400

    Create lolaucher.lua

---
+++ lolaucher.lua
@@ -0,0 +1,36 @@
+local lolaui = loadstring(game:HttpGet("https://raw.githubusercontent.com/lolaiuvt/lolaui/main/lolaui.lua"))()
+
+print("------------------------------")
+print("Welcome to | LoLauncher 1.2")
+print("LoLauncher is 100% FREE to use")
+print("Made by Lola Veries / iuvt")
+print("Made possible with lolaui")
+print("------------------------------")
+
+-- Category "General"
+
+print("button")
+
+-- Category "Da Hood"
+
+print("button2")
+
+-- Category "Pet Sim X"
+
+print("button3")
+
+-- Category "Arsenal"
+
+print("button4")
+
+-- Category "Phantom Forces"
+
+print("button5")
+
+-- Category "Client Mods"
+
+print("button6")
+
+-- Category "Uncategorized"
+
+print("button7")
